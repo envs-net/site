7a93dbeafb665ef79ce85d4a19cc3249bb4e15f5
creme <creme@envs.net>
2020-01-06 21:33:37 +0100

    add link to matrix-clients

--- chat/matrix/index.php
+++ chat/matrix/index.php
@@ -17,7 +17,7 @@ include '../../header.php';
 <pre>
 <h1><em>matrix chat</em></h1>
 
-our self-hosted matrix instance and web-client.
+our self-hosted matrix instance with riot-web client.
 
 <a href="https://matrix.envs.net/" target="_blank">matrix.envs.net</a>
 </pre>
@@ -33,11 +33,12 @@ An open network for secure, decentralized communication.
 <h3>&#35; matrix client software</h3>
 <a href="https://about.riot.im/downloads" target="_blank">check out the official riot client!</a>
 
+an extended client overview can be found at: <a href="https://matrix.org/clients/" target="_blank">https://matrix.org/clients/</a>
+
 
 <h3>&#35; links</h3>
 <a href="https://matrix.org/" target="_blank">matrix.org</a>
 <a href="https://riot.im/" target="_blank">riot.im</a>
-
 </pre>
 
       </div>
@@ -58,7 +59,7 @@ channel
 <pre>
 <table>
   <tr onclick="window.location='https://matrix.envs.net/';">
-    <td width="25px"><i class="fa fa-commenting-o fa-fw" aria-hidden="true"></i></td> <td><a href="https://matrix.envs.net/">web-client</a></td>
+    <td width="25px"><i class="fa fa-commenting-o fa-fw" aria-hidden="true"></i></td> <td><a href="https://matrix.envs.net/">riot-web client</a></td>
   </tr>
 </table></pre>
 </div>
