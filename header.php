25075c49dc408078f43f0737f94b6ebf851b57af
creme <creme@envs.net>
2020-02-02 13:27:13 +0100

    update header keywords

--- header.php
+++ header.php
@@ -7,7 +7,7 @@
     <meta name="author" content="Sven Kinne" />
     <meta name="robots" content="index, follow">
     <meta name="revisit-after" content="7 days">
-    <meta name="keywords" content="Sven Kinne,env,envs,enviroments,webpage,blog,gopher,forum,bbj,searx,news,feed,rss,atom,collaborative,pad,cryptpad,code,codepad,pastebin,pb,termbin,tb,file,curl,0x0,shorten,shorter,shortener,share,upload,ip,ifconfig,ipconfig,showip,whois,userspace,space,real-time,tiny,minimalist,shared,non-commercial,unix,linux,debian,bash,programmer,hackers,console,hosting" />
+    <meta name="keywords" content="Sven Kinne,env,envs,enviroments,tilde,tildeverse,tldr,userspace,space,real-time,tiny,minimalist,shared,non-commercial,linux,lxc,debian,shell,bash,programmer,hackers,console,hosting,selfhosting,openknowledge,decentralize,communication,fediverse,federated,social,webpage,blog,gopher,forum,bbj,search,searx,news,feed,rss,atom,collaborative,pad,cryptpad,code,codepad,paste,pastebin,pb,termbin,tb,file,curl,0x0,shorten,shorter,shortener,share,upload,pleroma,matrix,ip,ifconfig,ipconfig,showip,whois" />
     <link rel="stylesheet" href="/css/css_style.css" />
     <link rel="stylesheet" href="/css/fork-awesome.min.css" />
     <?=$additional_head ?? ""?>
