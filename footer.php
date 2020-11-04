fa047c0882c5db8bf25b549ee32dd928cbb8352b
creme <creme@envs.net>
2020-11-04 16:05:49 +0100

    add a impressum for envs

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <!-- FOOTER -->
-      <footer><pre class="clean">envs.net &copy; 2019-2020&nbsp;&nbsp;&#124;&nbsp;&nbsp;<a href="https://git.envs.net/envs/" target="_blank">source</a> by <a href="/~creme/">&#126;creme</a>&nbsp;&nbsp;&#124;&nbsp;&nbsp;contact: <a href="mailto:hostmaster@envs.net" target="_blank"><i class="fa fa-envelope-o fa-fw" aria-hidden="true"></i></a>&nbsp;&bull;&nbsp;<a href="/chat/matrix/"><i class="fa fa-matrix-org fa-fw" aria-hidden="true"></i></a></pre></footer>
+      <footer><pre class="clean">envs.net &copy; 2019-2020&nbsp;&#124;&nbsp;<a href="https://git.envs.net/envs/" target="_blank">source</a> by <a href="/~creme/">&#126;creme</a>&nbsp;&#124;&nbsp;<a href="/impressum/">impressum</a>&nbsp;&#124;&nbsp;contact: <a href="mailto:hostmaster@envs.net" target="_blank"><i class="fa fa-envelope-o fa-fw" aria-hidden="true"></i></a>&nbsp;&bull;&nbsp;<a href="/chat/matrix/"><i class="fa fa-matrix-org fa-fw" aria-hidden="true"></i></a></pre></footer>
 
     <?=$additional_footer ?? ""?>
     <?php unset($additional_footer); ?>
---
+++ impressum.php
@@ -0,0 +1,44 @@
+<?php
+  $title = "envs.net | impressum";
+  $desc = "envs.net | impressum";
+
+include 'header.php';
+?>
+
+  <body id="body" class="dark-mode">
+    <div>
+
+      <div class="button_back">
+        <pre class="clean"><strong><a href="/">&lt; back</a></strong></pre>
+      </div>
+
+      <div id="main">
+<div class="block">
+<h1><em>impressum</em></h1>
+<p></p>
+</div>
+
+<pre>
+responsible according to § 5 TMG:
+
+ Sven Kinne
+ Rudolfstraße 13
+ D-01097 Dresden
+</pre>
+
+<p></p>
+<table>
+  <tr><th class="tw130"></th> <th></th></tr>
+  <tr>
+    <td>website:</td>
+    <td><a href="https://envs.net/" target="_blank">https://envs.net/</a><br />
+    <a href="https://envs.sh/" target="_blank">https://envs.sh/</a></td>
+  </tr>
+  <tr>
+    <td>email:</td>
+    <td><a href="mailto:hostmaster@envs.net" target="_blank">hostmaster@envs.net</a></td>
+  </tr>
+</table>
+      </div>
+
+<?php include 'footer.php'; ?>
