28f0f9d7c3b2cd5b30bbdea8c541e9fdbf49cca3
creme <creme@envs.net>
2021-07-10 21:31:41 +0200

    update matrix community/space alias

--- code-of-conduct.php
+++ code-of-conduct.php
@@ -31,9 +31,9 @@ our community, as well as the consequences for unacceptable behavior.
 we invite all those who participate in envs.net to help us create safe and
 positive experiences for everyone.
 
-This applies to conversation in the any room in the matrix community (<a href="https://matrix.to/#/+envs:envs.net" target="_blank">+envs:envs.net</a>),
-also in the irc channel (#envs) on irc.tilde.chat, commits and comments relating to
-any project in <a href="https://github.com/envs-net" target="_blank">github</a>/<a href="https://git.envs.net/envs" target="_blank">gitea</a> spaces and as well as for any <a href="https://help.envs.net/#user-account-features" target="_blank">service</a> on envs.
+This applies to conversation in the any room in the matrix community (<a href="https://matrix.to/#space:envs.net" target="_blank">#space:envs.net</a>),
+also in the irc channel (#envs*) on irc.tilde.chat, commits and comments relating to
+any project in <a href="https://github.com/envs-net" target="_blank">github</a>/<a href="https://git.envs.net/envs" target="_blank">gitea</a> spaces and as well as for any <a href="https://help.envs.net/#user-account-features" target="_blank">services</a> on envs.
 </pre>
 <br />
 
