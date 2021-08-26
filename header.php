e6e4134d0e5d5372c3d6cdb1547fa4d8fafb0988
creme <creme@envs.net>
2021-08-26 18:41:39 +0200

    update meta keywords

--- header.php
+++ header.php
@@ -8,7 +8,7 @@
     <meta name="author" content="Sven Kinne" />
     <meta name="robots" content="index, follow" />
     <meta name="revisit-after" content="7 days" />
-    <meta name="keywords" content="envs,enviroments,env,tilde,tildeverse,tldr,userspace,space,real-time,tiny,minimalist,shared,non-commercial,linux,lxc,debian,shell,bash,programmer,hackers,console,hosting,selfhosting,openknowledge,decentralize,communication,fediverse,federated,social,webpage,blog,gemini,gopher,forum,bbj,search,searx,news,feed,rss,atom,collaborative,hedgedoc,codimd,pad,cryptpad,code,codepad,dot,git,gitea,irc,paste,pastebin,privatebin,pb,file,curl,nullpointer,0x0,shorten,shorter,shortener,share,upload,twtxt,getwtxt,tt-rss,pleroma,matrix,element,riot,hydrogen,dimension,jitsi,ip,ifconfig,ipconfig,showip,whois,znc" />
+    <meta name="keywords" content="envs,enviroments,env,tilde,tildeverse,tldr,userspace,space,real-time,tiny,minimalist,shared,non-commercial,linux,lxc,debian,shell,bash,programmer,hackers,console,hosting,selfhosting,openknowledge,decentralize,communication,ffsync,fediverse,federated,social,webpage,blog,gemini,gopher,forum,bbj,search,searx,news,feed,rss,atom,collaborative,hedgedoc,codimd,pad,cryptpad,code,codepad,dot,git,gitea,irc,paste,pastebin,privatebin,pb,file,curl,nullpointer,0x0,shorten,shorter,shortener,share,upload,twtxt,getwtxt,tt-rss,mobilizon,pleroma,matrix,element,riot,hydrogen,dimension,ip,ifconfig,ipconfig,showip,whois,znc" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="stylesheet" href="/css/css_style.css" />
     <link rel="stylesheet" href="/css/fork-awesome.min.css" />
