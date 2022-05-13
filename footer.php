afd897d4283b8333c882e392afaa870397e4fc67
creme <creme@envs.net>
2022-05-14 00:01:17 +0200

    update current year in footer .. looks like we have 2022 and not 2021

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <!-- FOOTER -->
-      <footer><pre class="clean">envs.net &copy; 2019-2021&nbsp;&#124;&nbsp;<a href="https://git.envs.net/envs/" target="_blank">source</a> by <a href="/~creme/">&#126;creme</a>&nbsp;&#124;&nbsp;<a href="/impressum/">impressum</a>&nbsp;&#124;&nbsp;contact: <a href="mailto:hostmaster@envs.net" target="_blank"><i class="fa fa-envelope-o fa-fw" aria-hidden="true"></i></a>&nbsp;&bull;&nbsp;<a href="/chat/matrix/"><i class="fa fa-matrix-org fa-fw" aria-hidden="true"></i></a></pre></footer>
+      <footer><pre class="clean">envs.net &copy; 2019-<?php echo date("Y"); ?>&nbsp;&#124;&nbsp;<a href="https://git.envs.net/envs/" target="_blank">source</a> by <a href="/~creme/">&#126;creme</a>&nbsp;&#124;&nbsp;<a href="/impressum/">impressum</a>&nbsp;&#124;&nbsp;contact: <a href="mailto:hostmaster@envs.net" target="_blank"><i class="fa fa-envelope-o fa-fw" aria-hidden="true"></i></a>&nbsp;&bull;&nbsp;<a href="/chat/matrix/"><i class="fa fa-matrix-org fa-fw" aria-hidden="true"></i></a></pre></footer>
 
     <?=$additional_footer ?? ""?>
     <?php unset($additional_footer); ?>
