8a39aadf4f5c597c6552e86fff37e4fe11a6a3c5
creme <creme@envs.net>
2022-09-20 10:24:43 +0200

    add mail service maintenance info

--- index.php
+++ index.php
@@ -31,9 +31,9 @@ include 'header.php';
 <br />
 
 <!-- maintenance info -->
-<!-- <pre class="alert">
-<i class="fa fa-exclamation-triangle fa-fw" aria-hidden="true"></i><strong>&nbsp; the matrix service is currently unavailable due to database maintenance.</strong>
-</pre> -->
+<pre class="alert">
+<i class="fa fa-exclamation-triangle fa-fw" aria-hidden="true"></i><strong>&nbsp; the mail service is currently unavailable due to maintenance.</strong>
+</pre>
 
 <table>
   <tr> <th class="tw140"></th> <th class="tw110"></th> <th></th> </tr>
