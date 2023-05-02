cf340a021bd26899ded06ec22e96624117fc3856
creme <creme@envs.net>
2023-05-02 22:26:20 +0200

    maybe we can now remove the BTC/ETC address from the footer

--- footer.php
+++ footer.php
@@ -1,7 +1,6 @@
 <!-- FOOTER -->
       <footer>
         <pre class="clean">envs.net &copy; 2019-<?php echo date("Y"); ?>&nbsp;&#124;&nbsp;<a href="https://git.envs.net/envs/" target="_blank">source</a> by <a href="/~creme/">&#126;creme</a>&nbsp;&#124;&nbsp;<a href="/impressum/">impressum</a>&nbsp;&#124;&nbsp;contact: <a href="mailto:hostmaster@envs.net" target="_blank"><i class="fa fa-envelope-o fa-fw" aria-hidden="true"></i></a>&nbsp;&bull;&nbsp;<a href="/chat/matrix/"><i class="fa fa-matrix-org fa-fw" aria-hidden="true"></i></a></pre>
-        <pre class="clean"><small>BTC: <code>bc1qxtljvxjjcrqt3kn8kl3pnwazny7k34kjxsyy7s</code> | ETH: <code>0xF481f6a7d9b22B3BE5d40A54C833A1C6eEdcdf69</code></small></pre>
       </footer>
 
     </div>
