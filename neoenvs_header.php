3bd24bfcf48eb215fc69416a7b1cfd397f0303ae
Slatian <baschdel@disroot.org>
2024-12-18 19:49:54 +0100

    stylesheet path from dev path to prod path

--- neoenvs_header.php
+++ neoenvs_header.php
@@ -10,7 +10,7 @@
 	<meta name="revisit-after" content="7 days" />
 	<meta name="keywords" content="envs,enviroments,tilde,tildeverse" />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
-	<link rel="stylesheet" href="/~slatian/neoenvs/css/neoenvs.css" />
+	<link rel="stylesheet" href="/css/neoenvs.css" />
 	<link rel="stylesheet" href="/css/fork-awesome.min.css" />
 	<?=$additional_head ?? ""?>
 	<?php unset($title); unset($desc); unset($additional_head); ?>
