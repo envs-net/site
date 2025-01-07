141cbaf1169eb4bc621c178b599254e8fc1dffd1
creme <creme@envs.net>
2025-01-07 16:38:18 +0100

    update chat/matrix

--- chat/matrix/index.php
+++ chat/matrix/index.php
@@ -26,15 +26,25 @@ include '../../neoenvs_header.php';
 		<p>our self-hosted matrix instance with web-clients.</p>
 
 		<p><i class="fa fa-matrix-org fa-fw" aria-hidden="true"></i> <a href="https://matrix.envs.net/" target="_blank">matrix.envs.net</a></p>
+		<p>
+			channel: <a href="https://matrix.to/#/#envs:envs.net" target="_blank"><code>#envs:envs.net</code></a> &#124;
+			space: <a href="https://matrix.to/#/#space:envs.net" target="_blank"><code>#space:envs.net</code></a>
+		</p>
 	</div>
 
 	<p>an open network for secure, decentralized communication.</p>
 
 	<p><a href="https://element.envs.net/#/register" target="_blank"><strong>&gt; create your matrix account on envs. &lt;</strong></a></p>
 
+
+	<h2>matrix&sol;element help &amp; documentation</h2>
+
+	<p><a href="https://matrix-help.envs.net/" target="_blank">check out our matrix&sol;element help page.</a></p>
+
+
 	<h2>identifiers/entities</h2>
 
-	<p>schema:<br>
+	<p>schema:<br />
 	&nbsp;&nbsp;<code>&lt;tag&gt;&lt;id&gt;:&lt;instance&gt;</code></p>
 
 	<ul>
@@ -53,11 +63,6 @@ include '../../neoenvs_header.php';
 	<p>there's a convenient site, <a href="https://matrix.to" target="_blank">matrix.to</a>, which will allow you to create urls linking to specific entities. when you visit these pages, you'll see links to communicate with those entities. If you enter <code>#envs:envs.net</code> on <code>matrix.to</code>, you'll get the URL <a href="https://matrix.to/#/#envs:envs.net" target="_blank">https://matrix.to/#/#envs:envs.net</a>.</p>
 
 
-	<h2>matrix&sol;element help &amp; documentation</h2>
-
-	<p><a href="https://matrix-help.envs.net/" target="_blank">check out our matrix&sol;element help page.</a></p>
-
-
 	<h2>matrix client software</h2>
 
 	<p><a href="https://element.io/get-started" target="_blank">check out the official element client!</a></p>
@@ -68,6 +73,7 @@ include '../../neoenvs_header.php';
 	<h2> privacy policy</h2>
 
 	<p>The privacy policy for the envs.net homeserver is available at <a href="https://matrix.envs.net/_matrix/consent" target="_blank">https://matrix.envs.net/_matrix/consent</a>.</p>
+
 
 	<h2>links</h2>
 
@@ -82,17 +88,17 @@ include '../../neoenvs_header.php';
 <div id="sidebar">
 
 	<div class="block">
-		<p>server admin<br>
+		<p>server admin<br />
 		<a href="https://matrix.to/#/@creme:envs.net" target="_blank"><code>@creme:envs.net</code></a></p>
 
-		<p>space<br>
+		<p>space<br />
 		<a href="https://matrix.to/#/#space:envs.net" target="_blank"><code>#space:envs.net</code></a></p>
 
-		<p>channel<br>
-		<a href="https://matrix.to/#/#envs:envs.net" target="_blank"><code>#envs:envs.net</code></a><br>
+		<p>channel<br />
+		<a href="https://matrix.to/#/#envs:envs.net" target="_blank"><code>#envs:envs.net</code></a><br />
 		<a href="https://matrix.to/#/#lounge:envs.net" target="_blank"><code>#lounge:envs.net</code></a></p>
 
-		<p>abuse management<br>
+		<p>abuse management<br />
 		<a href="https://matrix.to/#/#abuse:envs.net" target="_blank"><code>#abuse:envs.net</code></a></p>
 	</div>
 
